663bbc5fb9ac50b43e765b9522b1ee17c4e9aeff
Diyaa Daoud <diyaa97daoud@gmail.com>
2026-01-06 01:45:10 +0100

    fix polling in device details screen

--- app/src/main/java/com/example/waterlevelmonitoring/data/model/WaterLevelDataResponse.kt
+++ app/src/main/java/com/example/waterlevelmonitoring/data/model/WaterLevelDataResponse.kt
@@ -4,6 +4,8 @@ data class WaterLevelDataResponse(
     val id: Long,
     val deviceId: Long,
     val waterLevel: Double,
-    val pumpStatus: PumpStatus,
-    val timestamp: String
+    val pumpStatus: PumpStatus? = null,
+    val timestamp: String,
+    val temperature: Double? = null,
+    val humidity: Double? = null
 )
